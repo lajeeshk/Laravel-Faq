843bf45ac975d9dc3ce1aec771ef2b7b5b7b69b0
Lajeesh k <lajeeshk@gmail.com>
2020-02-11 20:38:35 +0530

    adding routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,49 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return redirect(app()->getLocale());
+});
+
+Route::get('/admin', function () {
+    return redirect(app()->getLocale().'/admin');
+});
+
+Route::get('/home', function () {
+    return redirect(app()->getLocale().'/home');
+});
+
+Route::get('/login', function () {
+    return redirect(app()->getLocale().'/login');
+});
+
+
+Route::group([
+    'prefix' => '{locale}', 
+    'where' => ['locale' => '[a-zA-Z]{2}'], 
+    'middleware' => 'setlocale'], function() {
+
+    Route::get('/', 'FaqController@index')->name('home');
+    
+    Auth::routes();
+    
+    Route::get('/home', 'HomeController@index')->name('home');
+
+    
+});
+
+Route::group(['prefix' => '{locale}/admin','middleware' => 'auth'], function() {
+    
+    Route::get('/', function (){
+        return view('admin/dashboard');
+    })->name('admin');
+
+    Route::resource('faq', 'Admin\FaqController',['as'=>'admin']);
+
+
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
